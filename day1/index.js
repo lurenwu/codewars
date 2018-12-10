26210fb18bdbe6e3ebb878b2d3bed59b4d5fe061
chenlin <378987691@qq.com>
2018-12-10 20:16:38 +0800

    day2

--- day1/index.js
+++ day1/index.js
@@ -2,10 +2,3 @@ function removeEveryOther(arr){
     return arr.filter(function(item,index){return index%2 === 0 })
  }
 
-Test.describe("Basic tests",_=>{
-Test.assertSimilar(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
-Test.assertSimilar(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
-Test.assertSimilar(removeEveryOther([[1, 2]]), [[1, 2]]);
-Test.assertSimilar(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
-Test.assertSimilar(removeEveryOther([]), []);
-})
---
+++ day2/index.js
@@ -0,0 +1,4 @@
+function litres(time) {
+    return  Math.ceil(time/2)
+}
+litres(3)
