a7c30d59259e232d9a43d8db052e132504c92c28
chenlin <378987691@qq.com>
2018-12-11 11:11:45 +0800

    day4

---
+++ day4/test.js
@@ -0,0 +1,14 @@
+
+var hero = require('./index.js');
+var Test = require('../test')
+var Test  = new Test();
+Test.describe('Fixed tests', _ => {
+  Test.assertEquals(hero(10, 5), true);
+  Test.assertEquals(hero(7, 4), false);
+  Test.assertEquals(hero(4, 5), false);
+  Test.assertEquals(hero(100, 40), true);
+  Test.assertEquals(hero(1500, 751), false);
+  Test.assertEquals(hero(0, 1), false);
+
+});
+  
