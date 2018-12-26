887a778801c1472c224a7274ecefbe7f8b1e055b
chenlin <378987691@qq.com>
2018-12-26 08:49:01 +0800

    day17

---
+++ day17/index.js
@@ -0,0 +1,5 @@
+
+function alphabetPosition(text) {
+    return text.match(/[a-zA-Z]/g) ?text.match(/[a-zA-Z]/g).map( (el) => el.toLowerCase().charCodeAt() - 96 ).join(' ') : '';
+  }
+module.exports = alphabetPosition
