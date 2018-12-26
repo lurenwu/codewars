6a4936f19db3d93da43b305ec8fd4e706ad70751
chenlin <378987691@qq.com>
2018-12-26 08:56:09 +0800

    day18

---
+++ day18/test.js
@@ -0,0 +1,21 @@
+var solution = require('./index.js');
+var Test = require('../test')
+var Test = new Test();
+
+Test.assertEquals(solution(1), 'I', '1 should, "I"')
+Test.assertEquals(solution(2), 'II', '2 should, "II"')
+Test.assertEquals(solution(3), 'III', '3 should, "III"')
+Test.assertEquals(solution(4), 'IV', '4 should, "IV"')
+Test.assertEquals(solution(5), 'V', '5 should, "V"')
+Test.assertEquals(solution(9), 'IX', '9 should, "IX"')
+Test.assertEquals(solution(10), 'X', '10 should, "X"')
+Test.assertEquals(solution(11), 'XI')
+Test.assertEquals(solution(19), 'XIX')
+Test.assertEquals(solution(22), 'XXII')
+Test.assertEquals(solution(15), 'XV')
+
+Test.assertEquals(solution(1000), 'M', '1000 should, "M"')
+Test.assertEquals(solution(1001), 'MI', '1001 should, "MI"')
+Test.assertEquals(solution(1990), 'MCMXC', '1990 should, "MCMXC"')
+Test.assertEquals(solution(2007), 'MMVII', '2007 should, "MMVII"')
+Test.assertEquals(solution(2008), 'MMVIII', '2008 should, "MMVIII"')
