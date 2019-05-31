d799e49d179f047bb2f30edcbe150f2ca380e102
chenlin <378987691@qq.com>
2019-05-31 16:54:39 +0800

    day23

---
+++ day23/test.js
@@ -0,0 +1,11 @@
+var duplicateCount = require('./index.js');
+var Test = require('../test')
+var Test = new Test();
+const assert = require("chai").assert;
+
+Test.assertEquals(duplicateCount(""), 0);
+Test.assertEquals(duplicateCount("abcde"), 0);
+Test.assertEquals(duplicateCount("aabbcde"), 2);
+Test.assertEquals(duplicateCount("aabBcde"), 2,"should ignore case");
+Test.assertEquals(duplicateCount("Indivisibility"), 1)
+Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
